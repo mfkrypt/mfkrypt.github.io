83b145f658525ffb8b1f27ab5ae7f244c2afbc35
mfkrypt <ammarsuper21@gmail.com>
2026-03-11 07:02:52 +0800

    Added Sincon 2025 to events

--- data/EventsData.ts
+++ data/EventsData.ts
@@ -17,11 +17,11 @@ const EventsData: EventItem[] = [
   
   {
     slug: 'HTB Meetup',
-    title: 'HTB Meetup @ GMI',
+    title: 'Speaker for HackTheBox Meetup @ GMI',
     date: '2025-07-26',
     location: 'Bangi, Malaysia',
     description:
-      'Had the honour of being the speaker for the HTB meetup hosted at GMI, the topic I presented was about Active Directory more specifically Kerberos for beginners to understand. Major thanks to GMI Cysec & ICE for having me.',
+      ' I Had the honour of being the speaker for the HTB meetup hosted at GMI, the topic I presented was about Active Directory more specifically Kerberos for beginners to understand. Major thanks to GMI Cysec & ICE for having me.',
     photos: [
       {
         src: '/static/images/events/htb-meetup/1.jpeg',
@@ -40,12 +40,36 @@ const EventsData: EventItem[] = [
   },
 
   {
+    slug: 'SINCON 2025',
+    title: 'Attending SINCON 2025',
+    date: '2025-05-22',
+    location: 'Orchard Road, Singapore',
+    description:
+      'I was fortunate enough to attend SINCON 2025, a cybersecurity conference held in Singapore. I explored RFID Hacking, AI Security and tried out the Active Directory range by The Range Village. Much thanks to Sherpasec for providing me with the ticket to attend this prestigious event.',
+    photos: [
+      {
+        src: '/static/images/events/sincon/1.jpeg',
+        alt: 'Placeholder',
+      },
+      {
+        src: '/static/images/events/sincon/2.jpeg',
+        alt: 'Placeholder',
+      },
+      {
+        src: '/static/images/events/sincon/3.jpeg',
+        alt: 'Placeholder',
+      },
+      
+    ],
+  },
+
+  {
     slug: 'GMI-Sharing-Session',
-    title: 'GMI Sharing Session',
+    title: 'Red & Blue Team Workshop Sharing Session @ GMI',
     date: '2025-02-23',
     location: 'Bangi, Malaysia',
     description:
-      'Did a technical sharing session on using C2 frameworks like Sliver for red teaming. Also did another beginner session on binary exploitation, tackling common pwn challenges in CTFs ',
+      ' I did a technical sharing session on using C2 frameworks like Sliver for red teaming. Also did another beginner session on binary exploitation, tackling common pwn challenges in CTFs ',
     photos: [
       {
         src: '/static/images/events/gmi-sharing/1.jpg',
@@ -69,7 +93,7 @@ const EventsData: EventItem[] = [
     date: '2024-11-29',
     location: 'Port Dickson, Malaysia',
     description:
-      'Got selected to participate in MCC2024 which honestly I did not expect. I had fun learning new stuff and meeting other cool, talented and down-to-earth people especially my idol, Capang. Major thanks to RE:HACK for giving me the opportunity to be part of this camp!',
+      'Was selected to participate in MCC2024 which honestly I did not expect. I had fun learning new stuff and meeting other cool, talented and down-to-earth people especially my idol, Capang. Major thanks to RE:HACK for giving me the opportunity to be part of this camp!',
     photos: [
       {
         src: '/static/images/events/mcc/2.jpg',
